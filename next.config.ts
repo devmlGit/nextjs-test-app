20a0c1a2e98d1fce63f2c99c42bcada5a307dc9a
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-09 12:52:02 +0100

    Set base path for the Next.js application

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-
+  basePath: '/nextjs-test-app'
 };
 
 export default nextConfig;
